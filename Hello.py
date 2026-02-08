6993983c8df49e2b2f1822d3d74db3ed864954e0
Palash1214 <palashwalde351@gmail.com>
2026-02-08 14:56:19 +0530

    Added python code succesfully

---
+++ Hello.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from my local Git repo!")
+    name = input("What is your name? ")
+    print(f"Nice to meet you, {name}. You've successfully pushed this code!")
+
+if __name__ == "__main__":
+    main()
